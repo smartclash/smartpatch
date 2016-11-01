181b27e9c2d2edb1c53d0b31dbd0592439111725
Alpha Man <smartclashcoc@gmail.com>
2016-11-01 20:30:20 +0530

    Working on codeigniter. Adding google OAuth

---
+++ application/config/smartpatch.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ramad
+ * Date: 01-Nov-16
+ * Time: 8:09 PM
+ */
+
+
+?>
---
+++ application/models/Database.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ramad
+ * Date: 01-Nov-16
+ * Time: 7:02 PM
+ */
+class Database
+{
+
+}
