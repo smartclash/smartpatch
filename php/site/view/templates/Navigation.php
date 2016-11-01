cb606333a25231f896c76409d4df32dd85f8b9ab
Alpha Man <smartclashcoc@gmail.com>
2016-11-01 18:47:58 +0530

    Added codeigniter

--- php/site/view/templates/Navigation.php
+++
@@ -1,6 +0,0 @@
-<li class="active">
-    <a href="index.html"><i class="fa fa-th-large"></i> <span class="nav-label">Main view</span></a>
-</li>
-<li>
-    <a href="minor.html"><i class="fa fa-th-large"></i> <span class="nav-label">Minor view</span> </a>
-</li>
