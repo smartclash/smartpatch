907d318b4221e6c336a8e77f09f5f812e10dec04
Alpha Man <smartclashcoc@gmail.com>
2016-11-01 18:22:40 +0530

    Delete the files
    Yuhu, let's recode frome base. No more bad scripts. We will go in OOP
    way with the help of codeigniter.

--- assets/php/scripts/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_unset("user");
-@setcookie("PatchyID","",time()-3600);
-
-echo "<h1>Logged out";
-echo "<br /> Redirecting.. back to homepage</h1><script> window.location.href='$fullPathToRoot'; </script>";
-die();
-?>
