90095dcb00682a83eda5eeb2feaddb8b690f3963
Alpha Man <smartclashcoc@gmail.com>
2016-12-03 16:40:33 +0000

    Forget OAuth, working on MYSQL login system :)

---
+++ application/models/Auth.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @owner xXAlphaManXx
+ * @editedOn 03/12/16
+ * @package Smart Patch
+ * @file Auth.php
+ *
+ * Licensed to Smart Hacks Inc.
+ * @link https://smarthacksinc.com
+ *
+ * @repo https://github.com/smarthacks/
+ *
+ */
+defined('ACCESS') OR exit('DIRECT SCRIPT ACCESS NOT ALLOWED');
+
+class Auth
+{
+
+}
