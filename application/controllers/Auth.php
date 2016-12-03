4b1c8eafe8dea9c3978d5a901713e783a54b1a29
Alpha Man <smartclashcoc@gmail.com>
2016-12-03 17:22:03 +0000

    Finished Register system :grin:

---
+++ application/controllers/Auth.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @owner xXAlphaManXx
+ * @editedOn 03/12/16
+ * @package Smart Patch
+ * @file Auth.php
+ *
+ * Licensed to Smart Hacks Inc.
+ * @link https://smarthacksinc.com
+ *
+ * @repo https://github.com/smarthacks/
+ *
+ */
+defined ('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth
+{
+
+}
