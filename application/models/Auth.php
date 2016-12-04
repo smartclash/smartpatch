846931da3138ed00d74a78100404c9e1e596aa34
Alpha Man <smartclashcoc@gmail.com>
2016-12-04 09:36:49 +0000

    Finished MYSQL login

--- application/models/Auth.php
+++ application/models/Auth.php
@@ -27,6 +27,37 @@ class Auth extends CI_Model {
 
     public function registerUser ($data)
     {
-        $this->db->insert('users', $data);
+        if( $this->db->insert('users', $data) ) {
+
+            $_SESSION['username'] = $data['username'];
+            $_SESSION['email']    = $data['email'];
+            $_SESSION['acctype']  = $data['acctype'];
+            $_SESSION['ip']       = $data['ip'];
+            $_SESSION['country']  = $data['country'];
+            $_SESSION['city']     = $data['city'];
+            $_SESSION['state']    = $data['state'];
+            $_SESSION['loggedIn'] = true;
+
+            $this->load->model('PageManager', 'director');
+            $this->director->pageDirector();
+        } else {
+            $this->load->view('auth/register', array('error' => 'Oops, something wrong has happened'));
+        }
+    }
+
+    public function checkUser($un, $em, $json)
+    {
+        $sql = "SELECT password, ip FROM users WHERE username='" . $un . "' OR email='" . $em . "'";
+        $query = $this->db->query($sql);
+        $rows = $query->num_rows();
+
+        if($rows === 1){
+            $data = array(
+                'error' => 'Oops, the username or email is already in use'
+            );
+            $this->load->view('auth/register', $data);
+        } else {
+            $this->registerUser($json);
+        }
     }
 }
---
+++ application/models/PageManager.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 04/12/16
+ * Time: 08:43
+ */
+class PageManager
+{
+
+}
